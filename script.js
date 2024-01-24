e1be22e546fdb7a5800bb5e37d358f84b6e47ef4
“willsmores” <scwillsmore@gmail.com>
2024-01-24 09:57:38 -0500

    Add animation and duration

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 console.log("Script link working")
 
-const createIcon = () => {
-  const icon = document.createElement("i");
+document.addEventListener("DOMContentLoaded", function() {
+  const createIcon = () => {
+    const icon = document.createElement("i");
 
-  // Add Fontawesome classes
-  icon.classList.add("icon", "fas", "fa-snowflake");
+    // Add Fontawesome classes
+    icon.classList.add("icon", "fas", "fa-snowflake");
 
-  // Randomise size of snowflake
-  const size = Math.random() + 1;
-  icon.style.fontSize = `${size}em`;
+    // Randomise size of snowflake
+    const size = Math.random() + 1;
+    icon.style.fontSize = `${size}em`;
 
-  // Randomise position of snowflake
-  const top = Math.random() * 100;
-  icon.style.top = `${top}vh`;
+    // Randomise position of snowflake
+    const top = Math.random() * 100;
+    icon.style.top = `${top}vh`;
 
-  const left = Math.random() * 100;
-  icon.style.left = `${left}vw`;
+    const left = Math.random() * 100;
+    icon.style.left = `${left}vw`;
 
-  //Randomise orientation of snowflake
-  const orientation = Math.random() * 360;
-  icon.style.transform = `rotate(${orientation}deg)`;
+    // Randomise orientation of snowflake
+    const orientation = Math.random() * 360;
+    icon.style.transform = `rotate(${orientation}deg)`;
 
-  document.body.appendChild(icon);
-}
+    // Randomise duration of animation
+    const duration = Math.random() * 5 + 2;
+    icon.style.animationDuration = `${duration}s`
 
-createIcon();
+    document.body.appendChild(icon);
+  }
+
+  createIcon();
+});
